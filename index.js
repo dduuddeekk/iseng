d483fc4c9dcebbb5b26a0c0ee44de970fda96f19
GungdeWiradana <gungde1967@gmail.com>
2023-12-29 22:34:35 +0800

    Add node,js.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hi, I'm Emu Ohtori");
+});
+
+app.listen(PORT, () => {
+  console.log("Server ready ....");
+});
